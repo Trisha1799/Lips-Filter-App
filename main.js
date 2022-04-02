033f42f95b2a5ad1c53e688b047cf14877e53473
Trisha1799 <84913232+Trisha1799@users.noreply.github.com>
2022-04-02 10:40:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function preload(){
 function setup(){
     canvas = createCanvas(300,300);
     canvas.center();
-    video.createCapture(VIDEO);
+    video = createCapture(VIDEO);
     video.size(300,300);
     video.hide();
 
